4018019bed899671fadfc93210eba13afcf87aba
Yohann <yzpt.de@gmail.com>
2024-02-05 22:13:41 +0100

    requets & manu bq insert ok

--- journal.sh
+++ journal.sh
@@ -4,12 +4,30 @@
 # API
 # https://metropole-europeenne-de-lille.opendatasoft.com/api/explore/v2.1/catalog/datasets/disponibilite-parkings/records?limit=100
 
-# 1. GCP project configuration
-
+# variables
 PROJECT_ID="parkings-mel"
 REGION="europe-west2"
 ZONE="europe-west2-a"
 SERVICE_ACCOUNT="admin-$PROJECT_ID"
+SERVICE_ACCOUNT_EMAIL=$SERVICE_ACCOUNT@$PROJECT_ID.iam.gserviceaccount.com
+DATA_BUCKET="gs://parkings-mel-data"
+FUNCTION_BUCKET="gs://parkings-mel-function"
+DATASET_NAME="parkings_mel"
+
+# git
+git init
+git add .
+git commit -m "first commit"
+git remote add origin https://github.com/yzpt/parkings-mel.git
+git push -u origin main
+
+# venv
+python3 -m venv venv
+source venv/bin/activate
+pip install google-cloud-bigquery 
+pip install google-cloud-storage
+
+# 1. GCP project configuration
 
 # create a new project
 gcloud projects create $PROJECT_ID
@@ -23,4 +41,33 @@ gcloud iam service-accounts create $SERVICE_ACCOUNT
 # grant permissions
 gcloud projects add-iam-policy-binding $PROJECT_ID --member="serviceAccount:$SERVICE_ACCOUNT@$PROJECT_ID.iam.gserviceaccount.com" --role="roles/bigquery.admin"
 gcloud projects add-iam-policy-binding $PROJECT_ID --member="serviceAccount:$SERVICE_ACCOUNT@$PROJECT_ID.iam.gserviceaccount.com" --role="roles/storage.admin"
+
+# create key for the service account
+gcloud iam service-accounts keys create key.json --iam-account=$SERVICE_ACCOUNT@$PROJECT_ID.iam.gserviceaccount.com
+
+
+# list billing accounts
+gcloud billing accounts list
+# Linking billing accout to the project
+gcloud billing projects link $PROJECT_ID --billing-account=016507-8E76F6-937B68
+
+
+# Data collection
+
+# Enabling APIs: Build, Functions, Pub/Sub, Scheduler
+gcloud services enable cloudfunctions.googleapis.com
+gcloud services enable pubsub.googleapis.com
+gcloud services enable cloudscheduler.googleapis.com
+
+# Creating GCS bucket for data collection
+gcloud storage buckets create $DATA_BUCKET
+# Creating GCS bucket for function storage
+gcloud storage buckets create $FUNCTION_BUCKET
+
+# BigQuery dataset
+bq mk $DATASET_NAME
+#delete dataset
+bq rm -r -f $DATASET_NAME
+
+# --> nb_bigquery.ipynb
 
